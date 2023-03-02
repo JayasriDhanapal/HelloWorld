5e769cd31ff29678a4ce78e15659b52da5620dac
JayasriDhanapal <126333274+JayasriDhanapal@users.noreply.github.com>
2023-03-02 15:23:58 +0530

    uploaded HelloWorld.java File

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+ public static void main(String[] args) {
+  // TODO Auto-generated method stub
+    System.out.println("Hello Agni");
+	    System.out.println("Hello Riya");
+
+ }
+
+}
